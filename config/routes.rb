eac4c750a9bf05d1712d35b972f52d17d7a5cd86
alainlap <alainlaplume@gmail.com>
2013-11-08 17:17:07 -0500

    Displaying validation errors on the product form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rainforest::Application.routes.draw do
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -12,7 +13,7 @@ Rainforest::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+    resources :products
 
   # Example resource route with options:
   #   resources :products do
